5bc782f19ca9e513f8c4b79038ca66c08c2b1ad2
Volev <arpacc@yandex.ru>
2021-10-26 15:53:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+class Main {
+    public static void main(String[] args) {
+        boolean isGreenLight = false;
+
+        int speedOfPlayer1 = 5;
+        int speedOfPlayer2 = 0;
+        int speedOfPlayer3 = 2;
+
+        // Допишите здесь логику так, что будет подсчитано и выведено
+        // количество игроков, которые выбывают.
+        // Если свет зелёный, то проходят все игроки (0 выбывают).
+        // Если свет красный, то выбывает каждый, чья скорость не 0.
+
+        int outPlayers = 0;
+
+        if(isGreenLight == false){
+            if(speedOfPlayer1 != 0){
+                outPlayers = outPlayers + 1;
+            }
+            if(speedOfPlayer2 != 0){
+                outPlayers = outPlayers + 1;
+            }
+            if(speedOfPlayer3 != 0){
+                outPlayers = outPlayers + 1;
+            }
+        }
+        if(isGreenLight == true){
+            if(speedOfPlayer1 == 0){
+                outPlayers = outPlayers + 1;
+            }
+            if(speedOfPlayer2 == 0){
+                outPlayers = outPlayers + 1;
+            }
+            if(speedOfPlayer3 == 0){
+                outPlayers = outPlayers +1;
+            }
+        }
+
+        System.out.print("Количество выбывших игроков: ");
+        System.out.print(outPlayers);
+
+    }
+}
