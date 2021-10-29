0b9265df7c14e3a1e64ff150e431beed09ca54ee
rovnanov <arpacc@yandex.ru>
2021-10-29 21:15:56 +0300

    Proval

--- src/Game.java
+++ src/Game.java
@@ -1,25 +1,37 @@
 public class Game {
 
-    public static void svetoFor(boolean isGreenLight, int speedOfPlayer1, int speedOfPlayer2, int speedOfPlayer3) {
-        int outPlayers = 0;
-        if(!isGreenLight){
-            if(speedOfPlayer1 != 0){
-                outPlayers = outPlayers + 1;
+    public static void svetoFor(boolean isGreenLight) {
+
+        String status1, status2, status3;
+        int playerSpeed1, playerSpeed2, playerSpeed3;
+        playerSpeed1 = 2; playerSpeed2 = 0; playerSpeed3 = 5;
+
+
+        if(!isGreenLight) {
+            if (playerSpeed1 != 0) {
+                System.out.println("Игрок 1 не выжил");
+                status1 = "Не выжил";
             }
-            if(speedOfPlayer2 != 0){
-                outPlayers = outPlayers + 1;
+            else System.out.println("Игрок 1 выжил"); status1 = "Выжил";
+            if (playerSpeed2 != 0) {
+                System.out.println("Игрок 2 не выжил");
+                status2 = "Не выжил";
             }
-            if(speedOfPlayer3 != 0){
-                outPlayers = outPlayers + 1;
+            else System.out.println("Игрок 2 выжил"); status2 = "Выжил";
+            if (playerSpeed3 != 0) {
+                System.out.println("Игрок 3 не выжил");
+                status3 = "Не выжил";
             }
+            else System.out.println("Игрок 3 выжил"); status3 = "Выжил";
         }
-        else outPlayers = 0;
-        System.out.println("Количество выбывающих игроков = " + outPlayers);
+        else System.out.println("Все игроки выжили");
 
     }
+
     public static void main(String[] args) {
-        svetoFor(false,2,0,5);
-
+        svetoFor(false);
     }
+
+
 
 }
---
+++ src/GameTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+    @Test
+    public void firstTest() {
+
+        boolean actual = Game.svetoFor(isGreenLight);
+
+    }
+}
