a1ea7fd75f3047c9b90d0ecc956a86ae8234d530
rovnanov <arpacc@yandex.ru>
2021-10-31 16:14:03 +0300

    added new version

--- src/Game.java
+++ src/Game.java
@@ -1,37 +1,22 @@
 public class Game {
+    public static boolean isGreen;
 
-    public static void svetoFor(boolean isGreenLight) {
+    public static boolean isLose(int speed) {
 
-        String status1, status2, status3;
-        int playerSpeed1, playerSpeed2, playerSpeed3;
-        playerSpeed1 = 2; playerSpeed2 = 0; playerSpeed3 = 5;
+        boolean result;
 
-
-        if(!isGreenLight) {
-            if (playerSpeed1 != 0) {
-                System.out.println("Игрок 1 не выжил");
-                status1 = "Не выжил";
-            }
-            else System.out.println("Игрок 1 выжил"); status1 = "Выжил";
-            if (playerSpeed2 != 0) {
-                System.out.println("Игрок 2 не выжил");
-                status2 = "Не выжил";
-            }
-            else System.out.println("Игрок 2 выжил"); status2 = "Выжил";
-            if (playerSpeed3 != 0) {
-                System.out.println("Игрок 3 не выжил");
-                status3 = "Не выжил";
-            }
-            else System.out.println("Игрок 3 выжил"); status3 = "Выжил";
+        if(isGreen == false){
+            if(speed != 0) result = false;
+            else result = true;
         }
-        else System.out.println("Все игроки выжили");
+        else result = true;
+        return result;
 
     }
-
     public static void main(String[] args) {
-        svetoFor(false);
+
+        boolean status = isLose(0);
+        System.out.println("Статус игрока = " + status);
+
     }
-
-
-
 }
