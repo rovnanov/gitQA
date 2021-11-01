9a3598dd0302f4b187d602fa9652ec1c034642fa
rovnanov <arpacc@yandex.ru>
2021-11-01 22:20:52 +0300

    added arrays game logic

--- src/Game.java
+++ src/Game.java
@@ -1,34 +1,92 @@
 public class Game {
     public static boolean isGreen;
 
-    public static boolean isLose(int speed) {
-
-        boolean result;
-
-        if (isGreen == false) {
-            if (speed != 0) {
-                result = false;
-            } else {
-                result = true;
+    public static int OutCount(int[] speed) {
+        int count = 0;
+        for (int i = 0; i < speed.length; i++) {
+            if (!isGreen) {
+                if (speed[i] != 0) {
+                    count = count + 1;
+                }
             }
-        } else {
-            result = true;
         }
-        return result;
+        return count;
+
+    }
+
+    public static int[] LoseArray(int[] speed) {
+        int LoseNumber = 0;
+        for (int i = 0; i < speed.length; i++) {
+            if (!isGreen) {
+                if (speed[i] != 0) {
+                    LoseNumber = LoseNumber + 1;
+                }
+            }
+        }
+
+        int[] LoseCountArray = new int[LoseNumber];
+        int o = 0;
+        for (int i = 0; i < speed.length; i++) {
+            if (!isGreen) {
+                if (speed[i] != 0) {
+                    LoseCountArray[o] = speed[i];
+                    o = o + 1;
+                } else {
+                    LoseCountArray[i] = speed[i + 1];
+                    i = i + 1;
+                    o = o + 1;
+                }
+            }
+        }
+        return LoseCountArray;
+    }
+
+    public static int[] WonArray(int[] speed) {
+        int WonNumber = 0;
+        for (int i = 0; i < speed.length; i++) {
+            if (!isGreen) {
+                if (speed[i] == 0) {
+                    WonNumber = WonNumber + 1;
+                }
+            } else {
+                WonNumber = WonNumber + 1;
+            }
+        }
+
+        int[] WonCountArray = new int[WonNumber];
+        int o = 0;
+        for (int i = 0; i < speed.length; i++) {
+            if (!isGreen) {
+                if (speed[i] == 0) {
+                    WonCountArray[o] = speed[i];
+                    o = o + 1;
+                }
+            } else {
+                WonCountArray[i] = speed[i];
+            }
+        }
+        return WonCountArray;
 
     }
 
     public static void main(String[] args) {
-
-        boolean status = isLose(1);
-        String condition;
-        if (status == false) {
-            condition = "Не выжил";
-        } else {
-            condition = "Выжил";
+        isGreen = false;
+        int[] speeds = {1, 0, 5, 10, -1};
+        int losecount = OutCount(speeds);
+        int[] lose = LoseArray(speeds);
+        int[] won = WonArray(speeds);
+        System.out.println("Количество выбывших = " + losecount);
+        System.out.println();
+        System.out.println("Массив из скоростей выбывающих: ");
+        for (int i = 0; i < lose.length; i++) {
+            System.out.println(lose[i]);
+        }
+        System.out.println();
+        System.out.println("Массив из скоростей не выбывающих: ");
+        for (int i = 0; i < won.length; i++) {
+            System.out.println(won[i]);
         }
 
-        System.out.println("Статус игрока = " + condition);
 
     }
 }
